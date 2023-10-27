09a46ae483133abf6542874bb3cce88bcbfe46b2
Michael <mwang4860@gmail.com>
2023-10-27 04:23:54 -0700

    feature: add support for overrides

--- Alarm_Bot.py
+++ Alarm_Bot.py
@@ -6,6 +6,7 @@ it acts as an alarm for when to go to sleep.
 
 import asyncio
 from datetime import datetime, timezone, timedelta
+import json
 import os
 
 import discord
@@ -17,25 +18,70 @@ intents = discord.Intents.all()
 intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-guild = discord.utils.get(bot.guilds, name=os.environ.get("ServerName"))
+
+"""
+Example Overrides.txt format:
+Don't let member with id "1234567890" join voice chat
+on Monday between 9am and 5pm Pacific Time
+(because they should be working :upsidedown-smile:)
+{
+    "1234567890":
+    {
+        "weekday": 1,
+        "hour_start": 9,
+        "hour_end": 17
+    }
+}
+"""
+async def load_overrides():
+    """
+    Loads an overrides file, which contains rules
+    that will work in addition to the default of "no voice from 3am to 8am"
+    """
+    with open("Overrides.txt", "r", encoding="UTF-8") as overrides_file:
+        return json.load(overrides_file)
+
+async def maybe_kick_member_from_voice(member: discord.Member):
+    """
+    Common function for usage in both 'run_loop()' and 'on_voice_state_update'
+    so that both functions respect the overrides file
+    """
+    current_time_utc = datetime.now(timezone.utc)
+    current_time_pacific = current_time_utc - timedelta(hours=7)
+    current_weekday = current_time_pacific.weekday()
+    current_hour = current_time_pacific.hour
+    overrides = await load_overrides()
+
+    member_id = str(member.id)
+    # No overrides present, follow the default rule
+    member_override = overrides[member_id]
+    if member_override is None or len(member_override) == 0:
+        if 3 <= current_hour <= 8:
+            await member.move_to(None)
+    else:
+        weekday_override = member_override.get("weekday")
+        hour_start_override = member_override.get("hour_start")
+        hour_end_override = member_override.get("hour_end")
+        if weekday_override is not None and hour_start_override is not None:
+            if current_weekday == weekday_override and current_hour == hour_start_override:
+                await member.move_to(None)
+        elif weekday_override is not None and hour_start_override is None:
+            if current_weekday == weekday_override:
+                await member.move_to(None)
+        elif weekday_override is None and hour_start_override is not None:
+            if hour_start_override <= current_hour < hour_end_override:
+                await member.move_to(None)
 
 async def run_loop():
     """ Main run loop of the bot """
     await bot.wait_until_ready()
+    guild = discord.utils.get(bot.guilds, name=os.environ.get("ServerName"))
 
     while not bot.is_closed():
-        current_time_utc = datetime.now(timezone.utc)
-        current_time_pacific = current_time_utc - timedelta(hours=7)
-
-        if current_time_pacific.hour < 3 or current_time_pacific.hour > 8:
-            await asyncio.sleep(60)
-            continue
-
         for channel in guild.voice_channels:
             for member in channel.members:
-                await member.move_to(None)
-
-        await asyncio.sleep(60)
+                await maybe_kick_member_from_voice(member)
+                await asyncio.sleep(60)
 
 @bot.event
 async def on_voice_state_update(member: discord.Member,
@@ -46,14 +92,10 @@ async def on_voice_state_update(member: discord.Member,
     Auto-kicks the user if the current time is between 3am and 8am Pacific Time
     """
 
-    if not after.channel:
+    if after.channel is None:
         return
 
-    current_time_utc = datetime.now(timezone.utc)
-    current_time_pacific = current_time_utc - timedelta(hours=7)
-
-    if 3 <= current_time_pacific.hour < 8:
-        await member.move_to(None)
+    await maybe_kick_member_from_voice(member)
 
 @bot.event
 async def on_ready():
